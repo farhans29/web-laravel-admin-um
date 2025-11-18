667165303d8a03eb9c7ef9a91ff7a108b6e54509
dneel27 <123617721+dneel27@users.noreply.github.com>
2025-11-18 15:58:59 +0700

    commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,9 +10,9 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
+    /*
      * The path to the "home" route for your application.
-     *
+*     *
      * Typically, users are redirected here after authentication.
      *
      * @var string
