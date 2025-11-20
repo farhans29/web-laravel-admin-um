a5265afeb67c272d8ae74e0742c46871d3f75c46
Hadrian Naufal <hadriannaufal10@gmail.com>
2025-11-20 15:15:23 +0700

    user fixing

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -455,7 +455,7 @@
                             @endcan
 
                             <!-- Invoices -->
-                            @can('view_invoices')
+                            {{-- @can('view_invoices')
                                 <li>
                                     <a href="{{ route('progress') }}"
                                         class="flex items-center gap-3 px-3 py-2 text-white rounded-lg hover:bg-indigo-700 transition-colors group relative">
@@ -474,7 +474,7 @@
                                         </div>
                                     </a>
                                 </li>
-                            @endcan
+                            @endcan --}}
 
                             <!-- Reports -->
                             @can('view_reports')
