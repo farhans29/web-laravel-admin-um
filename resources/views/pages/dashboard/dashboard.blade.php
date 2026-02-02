95a886ab946e45b0916d7fe7b53cca6f30c9559a
Hadrian Naufal <hadriannaufal10@gmail.com>
2026-02-02 12:35:06 +0700

    Update DashBoard 1.1

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -660,7 +660,7 @@
                                                     {{ $occupied['guest_name'] }}
                                                 </h4>
                                                 <p class="text-sm text-gray-600">{{ $occupied['room_name'] }} •
-                                                    {{ $occupied['room_type'] }}</p>
+                                                    {{ $occupied['room_number'] }}</p>
                                                 <p class="text-xs text-gray-500">{{ $occupied['property_name'] }}
                                                 </p>
                                             </div>
